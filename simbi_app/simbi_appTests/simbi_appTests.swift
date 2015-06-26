134b5c5cbdf2936c4f26f4455579a5dd9300ef50
rich-iannone <riannone@me.com>
2015-06-26 11:24:17 -0700

    Add initial project files

---
+++ simbi_app/simbi_appTests/simbi_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  simbi_appTests.swift
+//  simbi_appTests
+//
+//  Created by Richard Iannone on 2015-06-26.
+//  Copyright (c) 2015 simbi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class simbi_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
