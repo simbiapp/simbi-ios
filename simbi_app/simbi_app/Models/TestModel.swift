3c42710ecca10d08be7cef5a21c1b689b4a11186
Yang Cun <yangcun@live.com>
2015-06-29 05:08:32 +0800

    Add the MVC folders.

---
+++ simbi_app/simbi_app/Models/TestModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestModel.swift
+//  simbi_app
+//
+//  Created by YangCun on 6/29/15.
+//  Copyright (c) 2015 simbi. All rights reserved.
+//
+
+import UIKit
+
+class TestModel: NSObject {
+   
+}
---
+++ simbi_app/simbi_app/Views/TestView.swift
@@ -0,0 +1,21 @@
+//
+//  TestView.swift
+//  simbi_app
+//
+//  Created by YangCun on 6/29/15.
+//  Copyright (c) 2015 simbi. All rights reserved.
+//
+
+import UIKit
+
+class TestView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
